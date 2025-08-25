655ed4ec4e82b6bdb18815aaead0e2199bb10022
Andrey Starkov <starcowskyaaron@gmail.com>
2025-08-25 13:41:15 +0300

    fixes

--- src/widgets/widgets/scanner-table/ui/ScannerTable/components/TokenRow/components/TransactionsCell/index.tsx
+++ src/widgets/widgets/scanner-table/ui/ScannerTable/components/TokenRow/components/TransactionsCell/index.tsx
@@ -13,12 +13,12 @@ const TransactionsCellComponent: React.FC<TransactionsCellProps> = ({ transactio
 
   return (
     <TableCell>
-      <div className="flex flex-col items-start leading-tight">
-        <span className="font-mono text-white">{formatCount(total)}</span>
-        <div className="flex items-start justify-center gap-1 text-sm">
-          <span className="font-mono text-green-500">{formatCount(transactions.buys)}</span>
-          <span className="font-mono text-white/30">/</span>
-          <span className="font-mono text-red-600">{formatCount(transactions.sells)}</span>
+      <div className=" flex flex-col items-start justify-center text-center font-mono text-xs leading-tight">
+        <span style={{ fontSize: '11px' }} className="text-xs text-white">{formatCount(total)}</span>
+        <div className="flex items-start justify-center gap-1 text-center text-xs">
+          <span style={{ fontSize: '11px' }} className="font-mono text-xs text-green-500">{formatCount(transactions.buys)}</span>
+          <span style={{ fontSize: '11px' }} className="font-mono text-xs text-white/30">/</span>
+          <span style={{ fontSize: '11px' }} className="font-mono text-xs text-red-600">{formatCount(transactions.sells)}</span>
         </div>
       </div>
     </TableCell>
